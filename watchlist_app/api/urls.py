44fb81ec50c9aaa8fc5cd43caec6ab6a7449e04d
Shamseer-tt <shamseer.primalcodes@gmail.com>
2024-08-20 22:23:41 +0530

    class base api view setup

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path,include
-from watchlist_app.api.views import movie_list,movie_detail
+# from watchlist_app.api.views import movie_list,movie_detail
+from watchlist_app.api.views import MovieListView,MovieDetailView
 
 
 urlpatterns = [
-    path('list/' , movie_list, name='movie_list'),
-    path('<int:movie_id>', movie_detail, name='movie_id'),
+    path('list/' , MovieListView.as_view(), name='movie_list'),
+    path('<int:movie_id>', MovieDetailView.as_view(), name='movie_id'),
 ]
