394b6da88f5fc7e1d706398c651b3f8e23696fba
Shamseer-tt <shamseer.primalcodes@gmail.com>
2024-10-13 14:30:14 +0530

    jwt authentication

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path,include
 from rest_framework.authtoken.views import obtain_auth_token
 from user_app.api.views import register_user,logout_user
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
     path('register/',register_user, name='register' ),
-    path('logout/',logout_user, name='logout' )
+    path('logout/',logout_user, name='logout' ),
+    
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
