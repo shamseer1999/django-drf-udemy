ea970147b5da89f5294b6c5737812cf2bd487d1f
Shamseer-tt <shamseer.primalcodes@gmail.com>
2024-10-13 19:35:56 +0530

    changed settings to simple token method

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('register/',register_user, name='register' ),
     path('logout/',logout_user, name='logout' ),
     
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
